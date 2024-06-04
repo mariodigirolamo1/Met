78db93ba4b00a93df84aaddeee3c506380a8d555
mariodigirolamo1 <mariodigirolamowork@gmail.com>
2024-06-04 08:16:08 +0200

    feat: add basic bullets and player movement

--- Bullets/basic_player_bullet.gd
+++ Bullets/basic_player_bullet.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-const SPEED = 100
+const SPEED = 1200
 
 var direction = Vector2(0,0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position += direction * SPEED * delta
-	print(str(position))
+	rotation += 2 * delta
--- Levels/GreenArena/CharacterBody2D.gd
+++ Levels/GreenArena/CharacterBody2D.gd
@@ -1,20 +1,22 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 400.0
+const JUMP_VELOCITY = -700.0
+const SHOT_COOLDOWN_IN_SECONDS = 0.5
 
 signal bullet_fired(bulletDirection: Vector2, playerPosition: Vector2)
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var lastBulletDelay = 100
 
 func _physics_process(delta):
 	setVelocity(delta)
 	move_and_slide()
-	checkShots()
+	checkShots(delta)
 
 func setVelocity(delta):
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += 2.5 * gravity * delta
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -36,19 +38,22 @@ func getMovementDirection() -> int :
 		direction = 1
 	return direction
 	
-func checkShots():
-	var direction = getBulletDirection()
-	if(direction != Vector2(0,0)):
-		bullet_fired.emit(direction, position)
-		
+func checkShots(delta):
+	lastBulletDelay += delta
+	if(lastBulletDelay >= SHOT_COOLDOWN_IN_SECONDS):
+		var direction = getBulletDirection()
+		if(direction != Vector2(0,0)):
+			bullet_fired.emit(direction, position)
+			lastBulletDelay = 0
+	
 func getBulletDirection() -> Vector2 :
 	var bulletDirection = Vector2(0,0)
-	if(Input.is_action_just_pressed("FIRE_DOWN")):
+	if(Input.is_action_pressed("FIRE_DOWN")):
 		bulletDirection = Vector2(0,1)
-	if(Input.is_action_just_pressed("FIRE_UP")):
+	if(Input.is_action_pressed("FIRE_UP")):
 		bulletDirection = Vector2(0,-1)
-	if(Input.is_action_just_pressed("FIRE_LEFT")):
+	if(Input.is_action_pressed("FIRE_LEFT")):
 		bulletDirection = Vector2(-1,0)
-	if(Input.is_action_just_pressed("FIRE_RIGHT")):
+	if(Input.is_action_pressed("FIRE_RIGHT")):
 		bulletDirection = Vector2(1,0)
 	return bulletDirection
